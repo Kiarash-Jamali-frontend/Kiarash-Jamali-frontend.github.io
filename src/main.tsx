ecaba6cd368b1698972f0192ae73cc93ad49c4ca
Kiarash jamali <kiajamali978@gmail.com>
2025-12-19 16:06:18 +0330

    fix bugs in markdown

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'katex/dist/katex.min.css'
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
